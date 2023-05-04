5002d7e5f55e52a462abd5fd95230150c80b29d5
Danilo Matos <danilo.matos18@hotmail.com>
2023-05-04 09:07:09 -0300

    php basico

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+$nome= 'Danilo Matos';
+$idade= '21';
+
+
---
+++ linkar.php
@@ -0,0 +1,11 @@
+<?php
+//chamando o arquivo php de "index.php" usando a função "include"
+include 'index.php';
+
+echo "O $nome tem $idade anos de idade <br> <br>";
+
+//chamando o arquivo php de "index.php" usando a função "require"
+require 'index.php';
+
+echo "Sua idade é $idade anos, e seu nome é $nome";
+
