d73c7d6ef1c2adee9e000cc65da72616a69d7eb0
Danilo <danilo.matos18@hotmail.com>
2023-05-30 12:13:40 -0300

    php 7.0

--- oplogicos.php
+++ oplogicos.php
@@ -1,14 +1,28 @@
 <?php
 
-//&& = (e), || = (ou) 
+//&& = (e), || = (ou), !(NEGAÇÃO)
 
 $canAccess = true; //pode acessar
 $isOlder = false; //é mais velho
 
-/*$result = $canAccess && $isOlder; //os dois tem que ser verdadeiro, para que o resultado final seja verdadeiro
+/*$result = $canAccess && $isOlder; //os dois tem que ser verdadeiro, para que o resultado final seja verdadeiro, e vice versa para falso
 
 var_dump ($result);*/
 
-$result = $canAccess || $isOlder; // está sendo verificado se um dos dois é verdadeiro, se um dos dois for verdadeiro ele retorna verdadeiro
+//$result = $canAccess || $isOlder; // está sendo verificado se UM dos dois é verdadeiro, se UM dos dois for verdadeiro ele retorna verdadeiro
 
-var_dump ($result);
+//var_dump ($result);
+
+//var_dump(!$canAccess); //o "canAccess" é um valor (true), quando coloco "!", quando coloc um sinal de negação antes de um valor booleno, ele troca o valor, (no caso aqui transforma para falso)
+
+var_dump(!!$canAccess); //se colocar "!!" dois sinais, ele continua com o mesmo valor
+
+function ePar(int $n) {
+    if ($n % 2 != 0) {
+        return "é impar";
+    } 
+    return "é par";
+}
+
+echo ePar(4);
+
